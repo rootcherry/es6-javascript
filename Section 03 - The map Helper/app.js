575e073bbc6963bab61fa0087a269946f97e81ef
Chris N <hi.chrisn@gmail.com>
2021-11-13 11:32:41 -0300

    the map helper

---
+++ Section 03 - The map Helper/app.js
@@ -0,0 +1,17 @@
+// ES5
+var numbers = [1, 2, 3];
+var doubledNumbers = [];
+
+for (var i = 0; i < numbers.length; i++) {
+  doubledNumbers.push(numbers[i] * 2);
+}
+
+// console.log(doubledNumbers);
+
+// ES6
+let doubled = numbers.map(function(number) {
+  return number * 2;
+});
+
+console.log(doubled);
+
