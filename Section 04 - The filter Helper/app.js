0bcb59aa327f3254c167e6bddc8f463973c14f70
Chris N <hi.chrisn@gmail.com>
2021-12-27 17:36:24 -0300

    selecting needed data with filter

---
+++ Section 04 - The filter Helper/app.js
@@ -0,0 +1,24 @@
+var products = [
+    { name : 'cucumber', type: 'vegetable' },
+    { name : 'banana', type: 'fruit' },
+    { name : 'celery', type: 'vegetable' },
+    { name : 'orange', type: 'fruit' }
+];
+
+// ES5
+
+var filteredProducts = [];
+
+for (var i = 0; i < products.length; i++) {
+    if (products[i].type === 'fruit') {
+        filteredProducts.push(products[i]);
+    }
+}
+
+// console.log(filteredProducts);
+
+// ES6 - refactor
+
+products.filter(function(product) {
+    return product.type === 'fruit';
+});
