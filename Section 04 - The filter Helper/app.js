c7ebc243f61eada4e3b7730ac91ef5b72dd0e963
Chris N <hi.chrisn@gmail.com>
2021-12-28 15:14:18 -0300

    more on filtering

--- Section 04 - The filter Helper/app.js
+++ Section 04 - The filter Helper/app.js
@@ -22,3 +22,38 @@ for (var i = 0; i < products.length; i++) {
 products.filter(function(product) {
     return product.type === 'fruit';
 });
+
+// more on filtering
+
+var products = [
+    { name : 'cucumber', type: 'vegetable', quantity: 0, price: 1 },
+    { name : 'banana', type: 'fruit', quantity: 10, price: 15 },
+    { name : 'celery', type: 'vegetable', quantity: 30, price: 9 },
+    { name : 'orange', type: 'fruit', quantity: 3, price: 5 }
+];
+
+// Type is 'vegetable', quantity is greater than 0, price is less than 10
+
+const result = products.filter(function(product) {
+    return product.type === 'vegetable' 
+    && product.quantity > 0 
+    && product.price < 10;
+});
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
