84c0254dc111d809f8a321fcaada014a49097671
Chris N <hi.chrisn@gmail.com>
2022-01-02 00:40:35 -0300

    finding submitted users

---
+++ Section 06 - The every and some Helper/ex01.js
@@ -0,0 +1,11 @@
+// finding submitted users
+
+var users = [
+  { id: 21, hasSubmitted: true },
+  { id: 62, hasSubmitted: false },
+  { id: 4, hasSubmitted: true }
+];
+
+var hasSubmitted = users.every(function(user) {
+  return user.hasSubmitted === true;
+});
