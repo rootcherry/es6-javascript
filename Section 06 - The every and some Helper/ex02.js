2ee11b348c12981f5f9ca499b0fceb81dc3265e6
Chris N <hi.chrisn@gmail.com>
2022-01-02 00:43:17 -0300

    in progress network requests

---
+++ Section 06 - The every and some Helper/ex02.js
@@ -0,0 +1,11 @@
+// in progress network requests
+
+var requests = [
+  { url: '/photos', status: 'complete' },
+  { url: '/albums', status: 'pending' },
+  { url: '/users', status: 'failed' }
+];
+
+var inProgress = requests.some(function(req) {
+  return req.status === 'pending';
+});
