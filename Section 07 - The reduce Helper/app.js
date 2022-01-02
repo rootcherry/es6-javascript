7e386bf922734e029a0173e8646d4ccfed85361f
Chris N <hi.chrisn@gmail.com>
2022-01-02 01:06:28 -0300

    a touch more of reduce

--- Section 07 - The reduce Helper/app.js
+++ Section 07 - The reduce Helper/app.js
@@ -7,11 +7,29 @@ for (var i = 0; i < numbers.length; i++) {
   sum += numbers[i];
 }
 
-console.log(sum);
-console.log("---");
+// console.log(sum);
+// console.log("---");
 
 var result = numbers.reduce(function(sum, number) {
   return sum + number;
 }, 0);
 
-console.log(result);
+// console.log(result);
+
+// a touch more of reduce
+
+var primaryColors = [
+  { color: 'red' },
+  { color: 'yellow' },
+  { color: 'blue' },
+];
+
+// [ 'red', 'yellow', 'blue' ]; map/reduce
+
+var colors = primaryColors.reduce(function(previous, primaryColor) {
+  previous.push(primaryColor.color);
+
+  return previous;
+}, []);
+
+console.log(colors);
