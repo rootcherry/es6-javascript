89e7f49dba46baf61b6f648b0fc8b2753dadb6b7
Chris N <hi.chrisn@gmail.com>
2022-01-02 13:39:22 -0300

    fat arrow functions

---
+++ Section 10 - Arrow Functions/app.js
@@ -0,0 +1,17 @@
+// fat arrow functions
+
+// ES5
+
+const add = function(a, b) {
+  return a + b;
+}
+
+add(1, 2);
+
+// ES6
+
+const add = (a, b) => {  // const add = (a, b) => a + b;
+  return a + b;
+}
+
+add(1, 2);
