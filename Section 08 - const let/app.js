eb43e86fb338a33eb7a01e339a03578002ea0794
Chris N <hi.chrisn@gmail.com>
2022-01-02 12:57:20 -0300

    what const and let solve

--- Section 08 - const let/app.js
+++ Section 08 - const let/app.js
@@ -14,3 +14,20 @@ let hourlyWage = 40;
 
 title = 'Senior Software Engineer';
 hourlyWage = 45;
+
+// what const and let solve
+
+function count(targetString) {
+  var characters = ['a', 'e', 'i', 'o', 'u'];
+  var number = 0;
+
+  for (var i = 0; i < targetString.length; i++) {
+    if (characters.includes(targetString[i])) {
+      number++;
+    }
+  }
+
+  return number;
+}
+
+count('aeiobzxceiaipbiox');
