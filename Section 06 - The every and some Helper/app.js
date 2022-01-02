efa0c8a4e1dd473a53cc11b77e1adef6822b3487
Chris N <hi.chrisn@gmail.com>
2022-01-02 00:37:49 -0300

    every and some in practice

--- Section 06 - The every and some Helper/app.js
+++ Section 06 - The every and some Helper/app.js
@@ -47,3 +47,31 @@ names.every(function(name) {
 names.some(function(name) {
   return name.length > 4;
 });
+
+// every and some in practice
+
+function Field(value) {
+  this.value = value;
+}
+
+Field.prototype.validate = function () {
+  return this.value
+}
+
+var username = new Field("2cool");
+var password = new Field("my_password");
+var bday = new Field("10/10/2010");
+
+// username.validate() && password.validate();
+
+var fields = [username, password, bday];
+
+var formIsValid = fields.every(function(field) {
+  return field.validate();
+});
+
+if (formIsValid) {
+  // allow
+} else {
+  // error
+}
