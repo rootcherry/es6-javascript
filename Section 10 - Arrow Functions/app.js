72ee78c159b1283217be0f5977c77933563cb94b
Chris N <hi.chrisn@gmail.com>
2022-01-03 11:18:01 -0300

    when to use arrow functions

--- Section 10 - Arrow Functions/app.js
+++ Section 10 - Arrow Functions/app.js
@@ -25,3 +25,15 @@ const double = function(number) { // const double = number => 2 * number;
 const numbers = [1, 2, 3];
 
 numbers.map(number => 2 * number);
+
+// when to use arrow functions
+
+const team = {
+  members: ['Jane', 'Bill'],
+  teamName: 'Super Squad',
+  teamSummary: function() {
+    return this.members.map(function(member) {
+      return `${member} is on team ${this.teamName}`;
+    });
+  }
+};
