727f2e4c41c3b5f697566993c22a013045ce0f5c
Chris N <hi.chrisn@gmail.com>
2022-01-03 15:18:48 -0300

    refactor to use enhanced literal notation

---
+++ Section 11 - Enhanced Object Literals/ex04.js
@@ -0,0 +1,24 @@
+// refactor
+
+const color = 'red';
+
+// const Car = {
+//   color: color,
+//   drive: function() {
+//     return 'Vroom!';
+//   },
+//   getColor: function() {
+//     return this.color;
+//   }
+// };
+
+const Car = {
+  color,
+  drive() {
+    return 'Vroom!';
+  },
+  getColor() {
+    return this.color;
+  }
+};
+
