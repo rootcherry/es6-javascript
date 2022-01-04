df093596dab5b34c6f5a3872419f7980759d97c9
Chris N <hi.chrisn@gmail.com>
2022-01-04 13:46:19 -0300

    use cases of defaulting arguments

--- Section 12 - Default Function Arguments/app.js
+++ Section 12 - Default Function Arguments/app.js
@@ -12,3 +12,26 @@ function makeAjaxRequest(url, method = 'GET') {
 makeAjaxRequest('google.com');
 makeAjaxRequest('google.com', 'GET');
 makeAjaxRequest('google.com', 'POST');
+
+// use cases of defaulting arguments
+
+function User(id) {
+  this.id = id;
+}
+
+function generateId() {
+  return Math.random() * 9999999;
+}
+
+function createAdminUser(user = new User(generateId())) {
+  user.admin = true;
+
+  return user;
+}
+
+// createAdminUser(new User(generateId()));
+
+const user = new User(generateId());
+
+createAdminUser();
+createAdminUser(user);
