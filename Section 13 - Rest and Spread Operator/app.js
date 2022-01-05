4d998da5fd55b0a29564aecf49b1878667f4d15b
Chris N <hi.chrisn@gmail.com>
2022-01-04 23:42:01 -0300

    the rest on rest and spread

--- Section 13 - Rest and Spread Operator/app.js
+++ Section 13 - Rest and Spread Operator/app.js
@@ -7,3 +7,27 @@ function addNumbers(...numbers) {
 }
 
 addNumbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+// the rest on rest and spread
+
+const defaultColors = ['red', 'green'];
+const userFavoriteColors = ['orange', 'yellow'];
+const fallColors = ['fire red', 'fall orange'];
+
+// defaultColors.concat(userFavoriteColors);
+
+// refactor
+
+[ 'green', 'blue', ...fallColors, ...defaultColors, ...userFavoriteColors ];
+
+// validate shopping list
+
+function validateShoppingList(...items) {
+  if (items.indexOf('milk') < 0) {
+    return [ 'milk', ...items ];
+  }
+
+  return items;
+}
+
+validateShoppingList('oranges', 'bread', 'eggs');
