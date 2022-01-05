c88c70d8eb0470a117759e19df6ff1be887f0af1
Chris N <hi.chrisn@gmail.com>
2022-01-05 00:27:43 -0300

    goldmine of es6 - destructuring

---
+++ Section 14 - Destructuring/app.js
@@ -0,0 +1,16 @@
+// ES5
+
+var expense = {
+  type: 'Business',
+  amount: '$45 USD'
+};
+
+// var type = expense.type;
+// var amount = expense.amount;
+
+// ES6
+
+// const { type } = expense;
+// const { amount } = expense;
+
+const { type, amount } = expense;
