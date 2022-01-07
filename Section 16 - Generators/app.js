18f5f5dbb11eedea780d06b910047d63ca6e2d7a
Chris N <hi.chrisn@gmail.com>
2022-01-07 16:41:29 -0300

    generators with a short story

--- Section 16 - Generators/app.js
+++ Section 16 - Generators/app.js
@@ -18,10 +18,37 @@ for (let number of numbers) {
 
 // introduction to generators
 
-function* numbers() {
-  yield;
+// function* numbers() {
+//   yield;
+// }
+
+// const gen = numbers();
+// gen.next();
+// gen.next();
+
+
+// generators with a short story
+
+function* shopping() {
+  // stuff on the sidewalk
+
+  // walking down the sidewalk
+
+  // go into the store with cash
+
+  const stuffFromStore = yield 'cash';
+
+  // walking back home
+
+  return stuffFromStore;
 }
 
-const gen = numbers();
-gen.next();
-gen.next();
+// stuff in the store
+
+const gen = shopping();
+gen.next(); // leaving our house
+// walked into the store
+// walking up and down the aisles...
+// purchase our stuff
+
+gen.next('groceries'); // leaving the store with groceries
