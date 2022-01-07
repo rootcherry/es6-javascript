8b2d24c522768670bc387797455b705ee62ca8f2
Chris N <hi.chrisn@gmail.com>
2022-01-06 23:00:38 -0300

    prototypal inheritance

--- Section 15 - Classes/app.js
+++ Section 15 - Classes/app.js
@@ -10,5 +10,25 @@ Car.prototype.drive = function() {
 
 const car = new Car({ title: 'Focus' });
 
-console.log(car.drive());
-console.log(car);
+// console.log(car.drive());
+// console.log(car);
+
+// prototypal inheritance
+
+function Toyota(options) {
+  Car.call(this, options);
+  this.color = options.color;
+}
+
+Toyota.prototype = Object.create(Car.prototype);
+Toyota.prototype.constructor = Toyota;
+
+Toyota.prototype.honk = function() {
+  return 'beep';
+}
+
+const toyota = new Toyota({ color: 'red', title: 'Daily Driver' });
+console.log('---');
+console.log(toyota);
+console.log(toyota.drive());
+console.log(toyota.honk());
