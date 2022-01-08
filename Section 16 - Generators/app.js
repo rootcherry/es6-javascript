a9f90803d6ffd3e075f233849c3f73de020376f3
Chris N <hi.chrisn@gmail.com>
2022-01-08 15:04:06 -0300

    complexities of symbol.iterator

--- Section 16 - Generators/app.js
+++ Section 16 - Generators/app.js
@@ -91,19 +91,18 @@ const engineeringTeam = {
   department: 'Engineering',
   lead: 'Jill',
   manager: 'Alex',
-  engineering: 'Dave'
+  engineer: 'Dave',
+  [Symbol.iterator]: function* () {
+    yield this.lead;
+    yield this.manager;
+    yield this.engineer;
+    yield* this.testingTeam;
+  }
 };
-
-function* TeamIterator(team) {
-  yield team.lead;
-  yield team.manager;
-  yield team.engineering;
-  yield* team.testingTeam;
-}
 
 const names = [];
 
-for (let name of TeamIterator(engineeringTeam)) {
+for (let name of engineeringTeam) {
   names.push(name);
 }
 
