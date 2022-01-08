9267cb2ebdeb95e0959e6b99f9d57cb8f073de74
Chris N <hi.chrisn@gmail.com>
2022-01-08 14:31:30 -0300

    a pratical use of ES6 generators

--- Section 16 - Generators/app.js
+++ Section 16 - Generators/app.js
@@ -73,3 +73,27 @@ for (let color of colors()) {
 }
 
 console.log(myColors);
+
+// a pratical use of ES6 generators
+
+const engineeringTeam = {
+  size: 3,
+  department: 'Engineering',
+  lead: 'Jill',
+  manager: 'Alex',
+  engineering: 'Dave'
+};
+
+function* TeamIterator(team) {
+  yield team.lead;
+  yield team.manager;
+  yield team.engineering;
+}
+
+const names = [];
+
+for (let name of TeamIterator(engineeringTeam)) {
+  names.push(name);
+}
+
+console.log(names);
