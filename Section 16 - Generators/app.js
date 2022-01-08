a936843e69fbfbc08577d7d54a16e175d7dff94a
Chris N <hi.chrisn@gmail.com>
2022-01-08 15:24:53 -0300

    more on generators and recursion

--- Section 16 - Generators/app.js
+++ Section 16 - Generators/app.js
@@ -115,6 +115,13 @@ class Comment {
     this.content = content;
     this.children = children;
   }
+
+  *[Symbol.iterator]() {
+    yield this.content;
+    for (let child of this.children) {
+      yield* child; 
+    }
+  }
 }
 
 const children = [
@@ -124,4 +131,12 @@ const children = [
 ];
 
 const tree = new Comment('Great post!', children);
-console.log(tree);
+// console.log(tree);
+
+const values = [];
+
+for (let value of tree) {
+  values.push(value);
+}
+
+console.log(values);
