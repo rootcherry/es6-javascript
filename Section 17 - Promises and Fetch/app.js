5f689b9b2ab35d3e6752a731e4ee4987bcde366f
Chris N <hi.chrisn@gmail.com>
2022-01-08 16:41:55 -0300

    then and catch

--- Section 17 - Promises and Fetch/app.js
+++ Section 17 - Promises and Fetch/app.js
@@ -1,5 +1,12 @@
 // creating promises
 
 let promise = new Promise((resolve, reject) => {
-  
+  // reject();
+  resolve();
 });
+
+
+promise
+  .then(() => console.log('finally finished!'))
+  .then(() => console.log('i was also ran!!!'))
+  .catch(() => console.log('uh oh!!'));
